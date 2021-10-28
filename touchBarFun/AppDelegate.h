eb627fc9cc84c847bcd67aaf0d8478f876292a09
Ethan Chaffin <me@ethanrdoesmc.com>
2021-10-28 12:12:40 -0500

    Initial Commit

---
+++ touchBarFun/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  touchBarFun
+//
+//  Created by Ethan Chaffin on 10/28/21.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ touchBarFun/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  touchBarFun
+//
+//  Created by Ethan Chaffin on 10/28/21.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
